fb3ece9b0642ff1792b23243fe12c3ad5ba62619
andriw aparecido <andriw1919@gmail.com>
2023-07-11 00:17:09 -0300

    correção do bug de cadastri

--- php-action/post.php
+++ php-action/post.php
@@ -6,6 +6,7 @@ require_once 'C:\xampp\htdocs\php\Cadastro-de-Produtos\php-action\clear.php';
 
 
 function Cadastro($sql){
+
     global $connect;
     $query = mysqli_query($connect,$sql);
     if($query){
@@ -17,14 +18,14 @@ function Cadastro($sql){
 
 function verificacao(){
 
-    global $nome,$categoria;
-    $sql = "SELECT * FROM `pizzas` WHERE `nome` = '$nome'";
-    global $connect;
+    global $nome,$categoria, $connect;
+    $sql = "SELECT * FROM `pizzas` WHERE `nome` = '$nome';
     $query = mysqli_query($connect, $sql);
+
     if(mysqli_num_rows($query)>0){
-        return false;
-    }else{
         return true;
+    }else{
+        return false;
     }
 
 }
@@ -34,18 +35,17 @@ if(Isset($_POST['btn-enviar'])){
     // $imagem = clear($_POST['imgaem']);
     $nome = clear($_POST['nome']);
     $categoria = clear($_POST['categoria']);
-    $venda = clear($_POST['venda']);
+    $venda = floatval(clear($_POST['venda']));
     $custo = clear($_POST['custo']);
     $descricao = clear($_POST['descricao']);
-    
+    print_r($_POST);    
     
 
     // Cadastro 
-    if(verificacao()){
+    if(verificacao() == true){
         echo "produto já cadastrado";
     }else{
-        
-        $sql = "INSERT INTO `pizzas`(`nome`, `categoria`, `venda`, `custo`, `descricao`) VALUES ('$nome','$categoria','$venda','$custo','$descricao')";
+        $sql = "INSERT INTO `pizzas`(`nome`, `categoria`, `venda`, `custo`, `descricao`) VALUES ('$nome','$categoria',$venda,$custo,'$descricao')";
         Cadastro($sql);
         echo "cadastrado";
     }
