7944a7e00dd6c1987d37979307eb2c8280bfedde
andriw aparecido <andriw1919@gmail.com>
2023-09-02 22:56:38 -0300

    🔶add menu categorias

--- frontend/views/categoria/index.php
+++ frontend/views/categoria/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,7 +11,43 @@
     </style>
     <link rel="stylesheet" href="./css/style.css">
 </head>
+
 <body>
     <?php require_once '/xampp/htdocs/php/Cadastro-de-Produtos/frontend/layouts/menu-topo.php' ?>
+
+    <h1>MENU DE CATEGORIA</h1>
+
+    <form action="">
+        <input type="search" placeholder="Buscar categoria">
+        <div class="div-buttons">
+            <a href="/cadastro/categoria" class="button">cadastrar</a>
+            <button class="button">deletar</button>
+        </div>
+        <label class="label-selecionar" for="">selectionar todos<input type="checkbox"></label>
+        <div class="div-div-inputs">
+            <div class="div-inputs">
+                <input type="checkbox">
+                <label for="">NOME</label>
+            </div>
+            <div class="div-inputs">
+                <input type="checkbox">
+                <label for="">NOME</label>
+            </div>
+            <div class="div-inputs">
+                <input type="checkbox">
+                <label for="">NOME</label>
+
+            </div>
+            <div class="div-inputs">
+                <input type="checkbox">
+                <label for="">NOME</label>
+
+            </div>
+
+        </div>
+    </form>
+
+    <p><span style="color:red">clique</span> para atualizar ou ver sub categorias</p>
 </body>
+
 </html>
