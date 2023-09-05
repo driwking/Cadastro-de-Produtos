9d125760712d2d13d5f442ca57c0fd307f1b535e
andriw aparecido <andriw1919@gmail.com>
2023-09-05 16:08:51 -0300

    🔶add page cadastro de pizzas

--- frontend/views/Pizzas/index.php
+++ frontend/views/Pizzas/index.php
@@ -4,13 +4,53 @@
 <head>
     <meta charset='utf-8'>
     <meta name='viewport' content="width=device-width, initial-scale=1.0">
-    <link name='shortcut icon' href='/frontend/public/img/logo.png' type="image/x-icon">
-    <link name='stylesheet' href="/css/">
+    <link rel='shortcut icon' href='/frontend/public/img/logo.png' type="image/x-icon">
     <title>Pizzas</title>
     <style>
         <?php include_once '/xampp/htdocs/php/Cadastro-de-Produtos/frontend/layouts/css/menu-topo.css' ?>
     </style>
+    <link rel='stylesheet' href="./css/index.css">
 </head>
-    <?php require_once '/xampp/htdocs/php/Cadastro-de-Produtos/frontend/layouts/menu-topo.php' ?>
+<?php require_once '/xampp/htdocs/php/Cadastro-de-Produtos/frontend/layouts/menu-topo.php' ?>
+
+<h1>CADASTRO DE PIZZAS</h1>
+
+<form action="">
+    <div class="div-categorias">
+        <ul>
+            <li>
+                <label for="">NOME</label><input type="text" placeholder="xxxxx"></label>
+            </li>
+            <li>
+                <label for="">CATEGORIA PRINCIPAL</label>
+                <select type="text">
+                    <option value="selecione">selecione</option>
+                </select>
+            </li>
+            <li>
+                <label for="">CATEGORIA SEGUNDARIA</label>
+                <select type="text" placeholder="selecione">
+                    <option value="selecione">selecione</option>
+                </select>
+            </li>
+        </ul>
+    </div>
+    <div class="div-descricao">
+        <label for="">DESCRIÇÃO</label>
+        <textarea name="" id="" cols="30" rows="10" placeholder="xxxxx"></textarea>
+    </div>
+    <div class="div-imagem">
+        <label for="">IMAGEM</label>
+        <label for="img" class="label-input" id="lb-input">
+            <button>X</button>
+        </label>
+        <input type="file" name="img" id="img">
+    </div>
+    <div class="div-buttons">
+        <a href="">voltar ao cardapio</a>
+        <button>cadastrar</button>
+    </div>
+</form>
+<script src="./javascript/img.js"></script>
 
 </html>
