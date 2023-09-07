0f65082402ad11480c0dd7e8f7a1716c8c302f33
andriw aparecido <andriw1919@gmail.com>
2023-09-07 17:08:20 -0300

    🔶add pages adicionais

---
+++ frontend/views/adicionais/cadastroAdicionais.php
@@ -0,0 +1,40 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/frontend/public/img/logo.png" type="image/x-icon">
+    <title>Cadastro adicional</title>
+    <style>
+        <?php include_once $_SESSION['dir'] . 'frontend/layouts/css/menu-topo.css' ?>
+    </style>
+    <link rel="stylesheet" href="./css/cadastro.css">
+</head>
+
+<body>
+    <?php require_once $_SESSION['dir'] . 'frontend/layouts/menu-topo.php' ?>
+
+    <h1>CADASTRO DE ADICIONAL</h1>
+
+    <form action="">
+        <div class="div-inputs-1">
+            <label for="">NOME<input type="text" placeholder="XXXXXXX"></label>
+            <label for="">ITEMS<input type="text" placeholder="QUANTIDADE"></label>
+        </div>
+        <div class="div-inputs-2">
+            <label for="">TAMANHOS<input type="text" placeholder="UNICO"></label>
+            <label for="">VALORES<input type="text" placeholder="R$"></label>
+            <label for="">NOME ITEM<input type="text" placeholder="XXXXXXX"></label>
+        </div>
+
+        <div class="div-buttons">
+            <a href="/adicionais" class="btn-1">voltar ao cardapio</a>
+            <button class="btn-2">cadastrar</button>
+        </div>
+    </form>
+
+</body>
+
+</html>
---
+++ frontend/views/adicionais/index.php
@@ -0,0 +1,52 @@
+<?php session_start()?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Categorias</title>
+    <link rel="shortcut icon" href="/frontend/public/img/logo.png" type="image/x-icon">
+    <style>
+        <?php include_once $_SESSION['dir'].'/frontend/layouts/css/menu-topo.css' ?>
+    </style>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <?php require_once $_SESSION['dir'].'/frontend/layouts/menu-topo.php' ?>
+
+    <h1>MENU DE ADICIONAIS</h1>
+
+    <form action="">
+        <input type="search" placeholder="Buscar categoria">
+        <div class="div-buttons">
+            <a href="/adicionais/cadastro" class="button">cadastrar</a>
+            <button class="button">deletar</button>
+        </div>
+        <label class="label-selecionar" for="">selectionar todos<input type="checkbox"></label>
+        <div class="div-div-inputs">
+            <div class="div-inputs">
+                <input type="checkbox">
+               <label for=""><a href="/adicionais/editar">NOME</a></label>
+            </div>
+            <div class="div-inputs">
+                <input type="checkbox">
+                <label for=""><a href="/adicionais/editar">NOME</a></label>
+            </div>
+            <div class="div-inputs">
+                <input type="checkbox">
+                <label for=""><a href="/adicionais/editar">NOME</a></label>
+            </div>
+            <div class="div-inputs">
+                <input type="checkbox">
+                <label for=""><a href="/adicionais/editar">NOME</a></label>
+            </div>
+
+        </div>
+    </form>
+
+    <p><span style="color:red">clique</span> para atualizar ou ver adicionais</p>
+</body>
+
+</html>
