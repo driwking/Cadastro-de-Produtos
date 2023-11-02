f80645fb83ef2e2d89647c3bd360e16087e93906
andriw aparecido <andriw1919@gmail.com>
2023-11-02 20:18:31 -0300

    🍊updates of includes and requires

--- frontend/views/cadastro-produto/index.php
+++ frontend/views/cadastro-produto/index.php
@@ -1,4 +1,4 @@
-<?php session_start()?>
+<?php session_start() ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -8,13 +8,13 @@
     <title>Produtos</title>
     <link rel="shortcut icon" href="/frontend/public/img/logo.png" type="image/x-icon">
     <?php echo "<style>" ?>
-    <?php include_once $_SESSION['dir'].'/frontend/layouts/css/menu-topo.css' ?>
+    <?php include_once $_SESSION['layouts-css'].'menu-topo.css' ?>
     <?php echo "</style>" ?>
     <link rel="stylesheet" href="css/index.css">
 </head>
 
 <body>
-    <?php require_once $_SESSION['dir'].'/frontend/layouts/menu-topo.php' ?>
+    <?php require_once $_SESSION['layouts'].'menu-topo.php'?>
     <h1>CONHEÇA SEUS PRODUTOS</h1>
     <div class="produtos-main">
         <ul >
