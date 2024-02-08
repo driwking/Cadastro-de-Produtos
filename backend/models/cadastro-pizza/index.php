a485236083c8b4dea86a1fcc036635ddcd008aa5
andriw aparecido <andriw1919@gmail.com>
2024-02-07 23:19:46 -0300

    🍊test post

--- backend/models/cadastro-pizza/index.php
+++ backend/models/cadastro-pizza/index.php
@@ -6,13 +6,30 @@ $campo = [];
 for($i = 0 ; $i < count($_POST); $i++){
     
     if(isset($_POST[$i])){
-       $campo[$i] = clear($_POST[$i]);
+        if($i == 1 or $i == 2){
+
+            $k = 1;        
+            $campo[$k][$i]= clear($_POST[$i]);
+                 
+        }
+
+        // if(is_array($_POST[$i])){
+        //     for($j = 0; $j < array_count_values($_POST[$i]); $j++){
+        //         $campo[$i] = clear($_POST[$i][$j]);    
+        //     }
+        //     break;
+        // }
+        if($i != 1 and $i != 2){
+            $campo[$i] = clear($_POST[$i]);
+        }
     }
 }   
-;
+
+var_dump($campo);
+
 if(!verificacao($campo[0]) > 0){
 
-    create($campo[0],$campo[1],$campo[2]);
+    create($campo[0],$campo[1]);
     echo 'Produto cadastrado com sucesso';
 
 }else{
