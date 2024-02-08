547723cf3d3affa182df463bee458b1f1bb48805
andriw aparecido <andriw1919@gmail.com>
2024-02-07 23:24:58 -0300

    🌟add mesnage in page

--- frontend/views/categoria/editarCategoria.php
+++ frontend/views/categoria/editarCategoria.php
@@ -1,10 +1,10 @@
-<?php 
-session_start(); 
+<?php
+session_start();
 require_once $_SESSION['crud'];
 
 // POST FORM  
 
-echo $id = $_POST['id'];
+$id = $_POST['id'];
 $id_seq = $_POST['id_seq']
 
 ?>
@@ -18,21 +18,28 @@ $id_seq = $_POST['id_seq']
     <title>Editar categoria</title>
     <style>
         <?php include_once $_SESSION['layouts-css'] . 'menu-topo.css' ?>
-    </style>
+        </style>
     <link rel="stylesheet" href="./css/editar.css">
+    <link rel="stylesheet" href="./css/mensage.css">
 </head>
 
 <body>
-
+    
+    
     <?php require_once $_SESSION['layouts'] . 'menu-topo.php' ?>
-    <?php  var_dump($_POST) ?>
+    <!-- <?php var_dump($_POST) ?> -->
     <h1>EDITAR CATEGORIA</h1>
-    <form action="" method="POST">
+    <form action="../categoria/edit.php" method="POST" class="form-dados">
         <h2>PRINCIPAL</h2>
-        <input type="text" id="input-1" placeholder="tasf" value="<?php echo read($id_seq) ?>">
-        <div class="div-buttons">
+        <input type="text" id="input-1" placeholder="input-1" value="<?php echo read($id_seq) ?>">
+        <div class="div-buttons mensage-btn">
             <a class="button btn-1" href="/categorias">voltar ao menu</a>
-            <button class="button btn-2">atualizar</button>
+            <!-- <button class="button btn-2">atualizar</button> -->
+            <div class="div-btns button btn-2"> <!--- div-btns--->
+                <div onclick="send_mensage()" name="conf" type="submit" class="btn" placeholder="enviar" value="enviar">
+                    <p>enviar</p>
+                </div>
+            </div>
         </div>
         <div class="div-titulos">
             <h2>SEGUNDARIAS</h2>
@@ -41,14 +48,26 @@ $id_seq = $_POST['id_seq']
         </div>
         <div class="div-inputs">
 
-            <input type="text" placeholder="testeetst" value="<?php echo names_complet($id)[0] ?>">
-            <input type="text" placeholder="testeetst" value="<?php echo names_complet($id)[1] ?>">
-            <input type="text" placeholder="testeetst">
-
+            <input type="text" name="nome_primario" placeholder="input-2" value="<?php echo names_complet($id)[0] ?>">
+            <input type="text" name="nome_segundario" placeholder="input-3" value="<?php echo names_complet($id)[1] ?>">
+            <input type="text" name="nome_terciario" placeholder="input-4" >
         </div>
-        <button class="button btn-3">novo</button>
+        <div class="button btn-3">novo</div>
         <h3><span style="color:red">clique</span> para atualizar ou deletar</h3>
+
     </form>
-</body>
+    <div class="contaienr-bot"> <!--- bots--->
+        <div class="main-bot"> <!--- divs-btns--->
+            <h3 class="title-h3">
+                Deseja confirma a atualização dos campos de categoria ?
+            </h3>
+            <button onclick="conf()" name="conf" type="submit" class="btn_conf" value="confirmar">confirmar</button>
+            <div onclick="canc()" name="conf" class="btn_canc" value="confirmar">
+                <p>cancelar</p>
+            </div>
+        </div>
+    </div>
+    <script src="./js/index.js"></script>
+    </body>
 
 </html>
